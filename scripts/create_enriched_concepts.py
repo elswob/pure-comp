dbabe12264dc2bfb179f23a1777b4993f0a8b526
Ben Elsworth <be15516@bristol.ac.uk>
2017-05-04 13:26:49 +0100

    finished enrichment steps and added words to ignore

--- scripts/create_enriched_concepts.py
+++ scripts/create_enriched_concepts.py
@@ -2,20 +2,28 @@ import nltk_functions, neo4j_functions, stats_functions
 import operator,os
 from collections import defaultdict
 
+outDir='concepts/'
+
+def ignore_tokens():
+	#cat output/background_type_frequencies.txt | grep '/' | head -n 20 | cut -f1 | tr '\n' ','
+	l = ['/00a9','br/','/sub','/sup','/00b1','/00d7','/03bcm','/00b0c','/03b2','/em','a/03b2','/03b1','.','ltd.','elsevier','inc.']
+	return l
+
 def background_frequencies():
 	print "Getting background frequencies"
 	session = neo4j_functions.connect()
-	o1 = open('output/background_type_frequencies.txt', 'w')
+	o1 = open(outDir+'/background_type_frequencies.txt', 'w')
 	o1.write('type\tcount\n')
-	o2 = open('output/background_bigram_frequencies.txt', 'w')
+	o2 = open(outDir+'/background_bigram_frequencies.txt', 'w')
 	o2.write('bigram\tcount\n')
-	o3 = open('output/background_trigram_frequencies.txt', 'w')
+	o3 = open(outDir+'/background_trigram_frequencies.txt', 'w')
 	o3.write('trigram\tcount\n')
 	com = "match (s:Staff)--(p:Publication) return distinct p.abstract as a, p.title as t, p.pub_id as pid;"
 	typeDic = {}
 	bigramDic = {}
 	trigramDic = {}
 	counter=0
+	ignoreList = ignore_tokens()
 	for res in session.run(com):
 		if counter % 1000 == 0:
 			print counter
@@ -25,22 +33,27 @@ def background_frequencies():
 		#bigrams,trigrams = nltk_functions.bigrams_and_trigrams(abs)
 		bigrams,trigrams = nltk_functions.bigrams_and_trigrams(" ".join(types))
 		for s in set(types):
-			if s in typeDic:
-				typeDic[s]+=1
+			if s in ignoreList:
+				pass
 			else:
-				typeDic[s] = 1
+				if s in typeDic:
+					typeDic[s]+=1
+				else:
+					typeDic[s] = 1
 		for s in set(bigrams):
-			ss = s[0]+':'+s[1]
-			if ss in bigramDic:
-				bigramDic[ss]+=1
-			else:
-				bigramDic[ss] = 1
+			if len(set(s).intersection(ignoreList))==0:
+				ss = s[0]+':'+s[1]
+				if ss in bigramDic:
+					bigramDic[ss]+=1
+				else:
+					bigramDic[ss] = 1
 		for s in set(trigrams):
-			ss = s[0]+':'+s[1]+':'+s[2]
-			if ss in trigramDic:
-				trigramDic[ss]+=1
-			else:
-				trigramDic[ss] = 1
+			if len(set(s).intersection(ignoreList))==0:
+				ss = s[0]+':'+s[1]+':'+s[2]
+				if ss in trigramDic:
+					trigramDic[ss]+=1
+				else:
+					trigramDic[ss] = 1
 
 	for t in sorted(typeDic, key=typeDic.get, reverse=True):
 		o1.write(t+'\t'+str(typeDic[t])+'\n')
@@ -54,17 +67,17 @@ def background_frequencies():
 def person_frequencies():
 	print "Getting person frequencies"
 	session = neo4j_functions.connect()
-	o1 = open('output/person_type_frequencies.txt', 'w')
+	o1 = open(outDir+'/person_type_frequencies.txt', 'w')
 	o1.write('person\ttype\tcount\n')
-	o2 = open('output/person_bigram_frequencies.txt', 'w')
+	o2 = open(outDir+'/person_bigram_frequencies.txt', 'w')
 	o2.write('person\tbigram\tcount\n')
-	o3 = open('output/person_trigram_frequencies.txt', 'w')
+	o3 = open(outDir+'/person_trigram_frequencies.txt', 'w')
 	o3.write('person\ttrigram\tcount\n')
 	com = "match (s:Staff)--(p:Publication) return distinct s.published_name as p1, s.person_id as p2, p.abstract as a, p.title as t, p.pub_id as pid;"
 	typeDic = defaultdict(dict)
 	bigramDic = defaultdict(dict)
 	trigramDic = defaultdict(dict)
-
+	ignoreList = ignore_tokens()
 	counter=0
 	for res in session.run(com):
 		if counter % 1000 == 0:
@@ -78,22 +91,27 @@ def person_frequencies():
 		types = nltk_functions.tokenise_and_lemm(abs)
 		bigrams,trigrams = nltk_functions.bigrams_and_trigrams(" ".join(types))
 		for s in set(types):
-			if s in typeDic[person]:
-				typeDic[person][s]+=1
+			if s in ignoreList:
+				pass
 			else:
-				typeDic[person][s] = 1
+				if s in typeDic[person]:
+					typeDic[person][s]+=1
+				else:
+					typeDic[person][s] = 1
 		for s in set(bigrams):
-			ss = s[0]+':'+s[1]
-			if ss in bigramDic[person]:
-				bigramDic[person][ss]+=1
-			else:
-				bigramDic[person][ss] = 1
+			if len(set(s).intersection(ignoreList))==0:
+				ss = s[0]+':'+s[1]
+				if ss in bigramDic[person]:
+					bigramDic[person][ss]+=1
+				else:
+					bigramDic[person][ss] = 1
 		for s in set(trigrams):
-			ss = s[0]+':'+s[1]+':'+s[2]
-			if ss in trigramDic[person]:
-				trigramDic[person][ss]+=1
-			else:
-				trigramDic[person][ss] = 1
+			if len(set(s).intersection(ignoreList))==0:
+				ss = s[0]+':'+s[1]+':'+s[2]
+				if ss in trigramDic[person]:
+					trigramDic[person][ss]+=1
+				else:
+					trigramDic[person][ss] = 1
 
 	for p in sorted(typeDic, key=typeDic.get, reverse=True):
 		for t in sorted(typeDic[p], key=typeDic[p].get, reverse=True):
@@ -110,17 +128,18 @@ def person_frequencies():
 def org_frequencies():
 	print "Getting org frequencies"
 	session = neo4j_functions.connect()
-	o1 = open('output/org_type_frequencies.txt', 'w')
+	o1 = open(outDir+'/org_type_frequencies.txt', 'w')
 	o1.write('org\ttype\tcount\n')
-	o2 = open('output/org_bigram_frequencies.txt', 'w')
+	o2 = open(outDir+'/org_bigram_frequencies.txt', 'w')
 	o2.write('org\tbigram\tcount\n')
-	o3 = open('output/org_trigram_frequencies.txt', 'w')
+	o3 = open(outDir+'/org_trigram_frequencies.txt', 'w')
 	o3.write('org\ttrigram\tcount\n')
 	com = "match (o:Org)--(s:Staff)--(p:Publication) return distinct o.short_name as o1, o.code as o2, p.abstract as a, p.title as t, p.pub_id as pid;"
 	typeDic = defaultdict(dict)
 	bigramDic = defaultdict(dict)
 	trigramDic = defaultdict(dict)
 	counter=0
+	ignoreList = ignore_tokens()
 	for res in session.run(com):
 		if counter % 1000 == 0:
 			print counter
@@ -130,22 +149,27 @@ def org_frequencies():
 		types = nltk_functions.tokenise_and_lemm(abs)
 		bigrams,trigrams = nltk_functions.bigrams_and_trigrams(" ".join(types))
 		for s in set(types):
-			if s in typeDic[org]:
-				typeDic[org][s]+=1
+			if s in ignoreList:
+				pass
 			else:
-				typeDic[org][s] = 1
+				if s in typeDic[org]:
+					typeDic[org][s]+=1
+				else:
+					typeDic[org][s] = 1
 		for s in set(bigrams):
-			ss = s[0]+':'+s[1]
-			if ss in bigramDic[org]:
-				bigramDic[org][ss]+=1
-			else:
-				bigramDic[org][ss] = 1
+			if len(set(s).intersection(ignoreList))==0:
+				ss = s[0]+':'+s[1]
+				if ss in bigramDic[org]:
+					bigramDic[org][ss]+=1
+				else:
+					bigramDic[org][ss] = 1
 		for s in set(trigrams):
-			ss = s[0]+':'+s[1]+':'+s[2]
-			if ss in trigramDic[org]:
-				trigramDic[org][ss]+=1
-			else:
-				trigramDic[org][ss] = 1
+			if len(set(s).intersection(ignoreList))==0:
+				ss = s[0]+':'+s[1]+':'+s[2]
+				if ss in trigramDic[org]:
+					trigramDic[org][ss]+=1
+				else:
+					trigramDic[org][ss] = 1
 
 	for p in sorted(typeDic, key=typeDic.get, reverse=True):
 		for t in sorted(typeDic[p], key=typeDic[p].get, reverse=True):
@@ -164,7 +188,7 @@ def enrich_person():
 		print "Finding enriched person data for type "+i+" ..."
 		#get background frequencies
 		bDic = {}
-		with open('output/background_'+i+'_frequencies.txt', 'rb') as b:
+		with open(outDir+'/background_'+i+'_frequencies.txt', 'rb') as b:
 			next(b)
 			for line in b:
 				line = line.rstrip()
@@ -174,8 +198,8 @@ def enrich_person():
 		#enrich person data
 		e = defaultdict(dict)
 		counter=0
-		num_lines = sum(1 for line in open('output/person_'+i+'_frequencies.txt'))
-		with open('output/person_'+i+'_frequencies.txt', 'rb') as p:
+		num_lines = sum(1 for line in open(outDir+'/person_'+i+'_frequencies.txt'))
+		with open(outDir+'/person_'+i+'_frequencies.txt', 'rb') as p:
 			next(p)
 			#get num pubs per person
 			pDic = stats_functions.pubs_per_person()
@@ -206,7 +230,7 @@ def enrich_person():
 			e_cor[p] = stats_functions.multiple_test_correction(e[p])
 			#print d_mc
 			#print e
-		o = open('output/person_'+i+'_enriched.txt','w')
+		o = open(outDir+'/person_'+i+'_enriched.txt','w')
 		o.write('name\t'+i+'\ta1\ta2\tb1\tb2\todds\tpval\tcor-pval\n')
 		#print e_cor
 		for p in e_cor:
@@ -222,7 +246,7 @@ def enrich_orgs():
 		print "Finding enriched org data for type "+i+" ..."
 		#get background frequencies
 		bDic = {}
-		with open('output/background_'+i+'_frequencies.txt', 'rb') as b:
+		with open(outDir+'/background_'+i+'_frequencies.txt', 'rb') as b:
 			next(b)
 			for line in b:
 				line = line.rstrip()
@@ -232,8 +256,8 @@ def enrich_orgs():
 		#enrich org data
 		e = defaultdict(dict)
 		counter=0
-		num_lines = sum(1 for line in open('output/org_'+i+'_frequencies.txt'))
-		with open('output/org_'+i+'_frequencies.txt', 'rb') as p:
+		num_lines = sum(1 for line in open(outDir+'/org_'+i+'_frequencies.txt'))
+		with open(outDir+'/org_'+i+'_frequencies.txt', 'rb') as p:
 			next(p)
 			#get num pubs per person
 			pDic = stats_functions.pubs_per_org()
@@ -260,7 +284,7 @@ def enrich_orgs():
 			e_cor[p] = stats_functions.multiple_test_correction(e[p])
 			#print d_mc
 			#print e
-		o = open('output/org_'+i+'_enriched.txt','w')
+		o = open(outDir+'/org_'+i+'_enriched.txt','w')
 		o.write('name\t'+i+'\ta1\ta2\tb1\tb2\todds\tpval\tcor-pval\n')
 		#print e_cor
 		for p in e_cor:
@@ -273,93 +297,96 @@ def enrich_orgs():
 def add_enriched_to_graph():
 	print "Adding enrichment data to graph..."
 	session = neo4j_functions.connect()
-	counter=0
-	num_lines = sum(1 for line in open('output/person_type_enriched.txt'))
+	for concept_type in ['type','bigram','trigram']:
+		print "Adding data for data type '"+concept_type+"'"
+		counter=0
+		num_lines = sum(1 for line in open(outDir+'/person_'+concept_type+'_enriched.txt'))
 
-	#create concept nodes and index
-	with open('output/person_type_enriched.txt', 'rb') as p:
-		next(p)
-		for line in p:
-			line = line.rstrip()
-			#print line
-			if counter % 10000 == 0:
-				print str(counter)+'/'+str(num_lines)
-				session.close()
-				session = neo4j_functions.connect()
-			counter+=1
-			name,type,a1,a2,b1,b2,odds,pval,cor_pval = line.split('\t')
-			type = type.replace("'","\\'")
-			person_id = name.split(':')[1]
-			com = "MERGE (c:Concept {name:'"+type+"', type:'token'})"
-			#session.run(com)
+		#create concept nodes and index
+		with open(outDir+'/person_'+concept_type+'_enriched.txt', 'rb') as p:
+			next(p)
+			for line in p:
+				line = line.rstrip()
+				#print line
+				if counter % 10000 == 0:
+					print str(counter)+'/'+str(num_lines)
+					session.close()
+					session = neo4j_functions.connect()
+				counter+=1
+				name,type,a1,a2,b1,b2,odds,pval,cor_pval = line.split('\t')
+				type = type.replace("'","\\'")
+				person_id = name.split(':')[1]
+				com = "MERGE (c:Concept {name:'"+type+"', type:'"+concept_type+"'})"
+				session.run(com)
 
-	i="CREATE index on :Concept(name);"
+		i="CREATE index on :Concept(name);"
+		session.run(i)
+
+		counter=0
+		#create person-concept relationships
+		print "Creating person-concept relationships..."
+		with open(outDir+'/person_'+concept_type+'_enriched.txt', 'rb') as p:
+			next(p)
+			for line in p:
+				line = line.rstrip()
+				#print line
+				if counter % 10000 == 0:
+					print str(counter)+'/'+str(num_lines)
+					session.close()
+					session = neo4j_functions.connect()
+				counter+=1
+				name,type,a1,a2,b1,b2,odds,pval,cor_pval = line.split('\t')
+				if float(cor_pval)>0:
+					cor_pval = ("%03.02e" % float(cor_pval))
+				else:
+					cor_pval = 0.0
+				type = type.replace("'","\\'")
+				person_id = name.split(':')[1]
+				com = "MATCH (s:Staff {person_id: "+person_id+"}) " \
+					  "MATCH (c:Concept {name:'"+type+"',type:'"+concept_type+"'}) " \
+					  "MERGE (s)-[:ENRICHED{type:'pure-comp',year:2014,localCount:"+str(a1)+"," \
+					  "localTotal:"+str(a2)+",globalCount:"+str(b1)+",globalTotal:"+str(b2)+",cpval:"+str(cor_pval)+"}]-(c);"
+				session.run(com)
+
+		counter=0
+		#create org-concept relationships
+		print "Creating org-concept relationships..."
+		with open(outDir+'/org_'+concept_type+'_enriched.txt', 'rb') as p:
+			next(p)
+			for line in p:
+				if counter % 10000 == 0:
+					print counter
+					session.close()
+					session = neo4j_functions.connect()
+				counter+=1
+				line = line.rstrip()
+				name,type,a1,a2,b1,b2,odds,pval,cor_pval = line.split('\t')
+				if float(cor_pval)>0:
+					cor_pval = ("%03.02e" % float(cor_pval))
+				else:
+					cor_pval = 0.0
+				type = type.replace("'","\\'")
+				code = name.split(':')[1]
+				com = "MATCH (o:Org {code: '"+code+"'}) " \
+					  "MATCH (c:Concept {name:'"+type+"',type:'"+concept_type+"'}) " \
+					  "MERGE (o)-[:ENRICHED{type:'pure-comp',year:2014,localCount:"+str(a1)+"," \
+					  "localTotal:"+str(a2)+",globalCount:"+str(b1)+",globalTotal:"+str(b2)+",cpval:"+str(cor_pval)+"}]-(c);"
+				#print com
+				session.run(com)
+	i="CREATE index on :Concept(type);"
 	session.run(i)
-
-	counter=0
-	#create person-concept relationships
-	print "Creating person-concept relatioships..."
-	with open('output/person_type_enriched.txt', 'rb') as p:
-		next(p)
-		for line in p:
-			line = line.rstrip()
-			#print line
-			if counter % 10000 == 0:
-				print str(counter)+'/'+str(num_lines)
-				session.close()
-				session = neo4j_functions.connect()
-			counter+=1
-			name,type,a1,a2,b1,b2,odds,pval,cor_pval = line.split('\t')
-			if float(cor_pval)>0:
-				cor_pval = ("%03.02e" % float(cor_pval))
-			else:
-				cor_pval = 0.0
-			type = type.replace("'","\\'")
-			person_id = name.split(':')[1]
-			com = "MATCH (s:Staff {person_id: "+person_id+"}) " \
-				  "MATCH (c:Concept {name:'"+type+"'}) " \
-				  "MERGE (s)-[:ENRICHED{type:'pure-comp',year:2014,localCount:"+str(a1)+"," \
-				  "localTotal:"+str(a2)+",globalCount:"+str(b1)+",globalTotal:"+str(b2)+",cpval:"+str(cor_pval)+"}]-(c);"
-			#session.run(com)
-
-	counter=0
-	#create org-concept relationships
-	print "Creating org-concept relatioships..."
-	with open('output/org_type_enriched.txt', 'rb') as p:
-		next(p)
-		for line in p:
-			if counter % 10000 == 0:
-				print counter
-				session.close()
-				session = neo4j_functions.connect()
-			counter+=1
-			line = line.rstrip()
-			name,type,a1,a2,b1,b2,odds,pval,cor_pval = line.split('\t')
-			if float(cor_pval)>0:
-				cor_pval = ("%03.02e" % float(cor_pval))
-			else:
-				cor_pval = 0.0
-			type = type.replace("'","\\'")
-			code = name.split(':')[1]
-			com = "MATCH (o:Org {code: '"+code+"'}) " \
-				  "MATCH (c:Concept {name:'"+type+"'}) " \
-				  "MERGE (o)-[:ENRICHED{type:'pure-comp',year:2014,localCount:"+str(a1)+"," \
-				  "localTotal:"+str(a2)+",globalCount:"+str(b1)+",globalTotal:"+str(b2)+",cpval:"+str(cor_pval)+"}]-(c);"
-			#print com
-			session.run(com)
-
 	session.close()
 
 if __name__ == '__main__':
-	if os.path.exists('output/background_type_frequencies.txt'):
+	if os.path.exists(outDir+'/background_type_frequencies.txt'):
 		print 'Background frequencies already created'
 	else:
 		background_frequencies()
-	if os.path.exists('output/person_type_frequencies.txt'):
+	if os.path.exists(outDir+'/person_type_frequencies.txt'):
 		print 'Person frequencies already created'
 	else:
 		person_frequencies()
-	if os.path.exists('output/org_type_frequencies.txt'):
+	if os.path.exists(outDir+'/org_type_frequencies.txt'):
 		print 'Org frequencies already created'
 	else:
 		org_frequencies()
@@ -367,4 +394,4 @@ if __name__ == '__main__':
 	#run enrichment steps
 	enrich_person()
 	enrich_orgs()
-	#add_enriched_to_graph()
+	add_enriched_to_graph()
