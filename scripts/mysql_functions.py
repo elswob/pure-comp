bbf06302f5bc5da4d5965393de9a431bad3cf17c
Ben Elsworth <be15516@bristol.ac.uk>
2017-05-05 10:57:49 +0100

    added mysql functions

---
+++ scripts/mysql_functions.py
@@ -0,0 +1,5 @@
+import mysql.connector
+
+cnx = mysql.connector.connect(user='elswob', password='badger',
+                              host='127.0.0.1',
+                              database='arse')
--- scripts/publication_metrics.py
+++ scripts/publication_metrics.py
@@ -1,25 +1,52 @@
 import neo4j_functions, nltk_functions
 from time import gmtime, strftime
+import os
 
 def get_metrics():
+
+	#check for previous
+	pubDic = {}
+	if os.path.exists('output/pubMetrics.txt'):
+		with open('output/pubMetrics.txt', 'rb') as p:
+			for line in p:
+				if len(line.split('\t'))==13:
+					pid,cf,ld,ts_flesch_reading_ease,ts_smog_index,ts_flesch_kincaid_grade,ts_coleman_liau_index,ts_automated_readability_index,ts_dale_chall_readability_score,ts_difficult_words,ts_linsear_write_formula,ts_gunning_fog,ts_text_standard = line.split('\t')
+					pubDic[int(pid)]=''
+	#print pubDic
+	print "Found "+str(len(pubDic))+" entries already processed"
 	session = neo4j_functions.connect()
-	com = "match (p:Publication) return distinct p.abstract as a, p.title as t, p.pub_id as pid;"
+	com = "match (p:Publication) return distinct p.abstract as a, p.pub_id as pid;"
 	counter=0
+	o = open('output/pubMetrics.txt','a')
 	for res in session.run(com):
 		if counter % 1000 == 0:
 			print counter, strftime("%Y-%m-%d %H:%M:%S", gmtime())
-			session.close()
-			session = neo4j_functions.connect()
+			#session.close()
+			#session = neo4j_functions.connect()
 		counter+=1
 		abs = res['a']
 		pid = res['pid']
-		#content fraction
-		if len(abs)>0:
-			cf = nltk_functions.content_fraction(abs)
-			ld = nltk_functions.lexical_diversity(abs)
-			ts = nltk_functions.run_textstat(abs)
-			ts_flesch_reading_ease,ts_smog_index,ts_flesch_kincaid_grade,ts_coleman_liau_index,ts_automated_readability_index,ts_dale_chall_readability_score,ts_difficult_words,ts_linsear_write_formula,ts_gunning_fog,ts_text_standard = ts
-			#print pid, cf, ld, ts_flesch_reading_ease
+		#print pid
+		if pid not in pubDic:
+			#content fraction
+			if len(abs)>0:
+				cf = nltk_functions.content_fraction(abs)
+				ld = nltk_functions.lexical_diversity(abs)
+				ts = nltk_functions.run_textstat(abs)
+				ts_flesch_reading_ease,ts_smog_index,ts_flesch_kincaid_grade,ts_coleman_liau_index,ts_automated_readability_index,ts_dale_chall_readability_score,ts_difficult_words,ts_linsear_write_formula,ts_gunning_fog,ts_text_standard = ts
+				#print pid, cf, ld, ts_flesch_reading_ease
+				o.write(str(pid)+"\t"+str("%.4f" % cf)+"\t"+str("%.4f" % ld)+"\t"+str("%.4f" % ts_flesch_reading_ease)+""
+					   "\t"+str("%.4f" % ts_smog_index)+"\t"+str("%.4f" % ts_flesch_kincaid_grade)+"\t"+str("%.4f" % ts_coleman_liau_index)+""
+					   "\t"+str("%.4f" % ts_automated_readability_index)+"\t"+str("%.4f" % ts_dale_chall_readability_score)+""
+					   "\t"+str("%.4f" % ts_difficult_words)+"\t"+str("%.4f" % ts_linsear_write_formula)+"\t"+str("%.4f" % ts_gunning_fog)+""
+					   "\t"+str(ts_text_standard)+"\n")
+
+def add_metrics_to_graph():
+	session = neo4j_functions.connect()
+	with open('output/pubMetrics.txt', 'rb') as p:
+		for line in p:
+			line = line.rstrip()
+			pid,cf,ld,ts_flesch_reading_ease,ts_smog_index,ts_flesch_kincaid_grade,ts_coleman_liau_index,ts_automated_readability_index,ts_dale_chall_readability_score,ts_difficult_words,ts_linsear_write_formula,ts_gunning_fog,ts_text_standard = line.split('\t')
 			com = "match (p:Publication) where p.pub_id = "+str(pid)+" set p.cf = "+str("%.4f" % cf)+",p.ld ="+str("%.4f" % ld)+"" \
 				   ",p.ts_flesch_reading_ease="+str("%.4f" % ts_flesch_reading_ease)+",p.ts_smog_index="+str("%.4f" % ts_smog_index)+",p.ts_flesch_kincaid_grade="+str("%.4f" % ts_flesch_kincaid_grade)+"" \
 			       ",p.ts_coleman_liau_index="+str("%.4f" % ts_coleman_liau_index)+",p.ts_automated_readability_index="+str("%.4f" % ts_automated_readability_index)+"" \
