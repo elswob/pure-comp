dff87501589ef8f4406eed6f99c86289b2c2e234
Ben Elsworth <be15516@bristol.ac.uk>
2017-06-28 17:21:10 +0100

    dealing with pure data

--- scripts/get_pure_data.py
+++ scripts/get_pure_data.py
@@ -2,18 +2,19 @@ import requests
 import os
 import json
 import gzip
+import re
 import xmltodict
 from time import gmtime, strftime
 
 dataDir = '/Users/be15516/projects/pure-comp/data/'
+fSize=1000
+totalPubs = 160000
 
 def is_non_zero_file(fpath):
     return os.path.isfile(fpath) and os.path.getsize(fpath) > 35
 
 def get_xml():
 	print "Getting pure xml"
-	fSize=1000
-	totalPubs = 160000
 	for i in range(0,totalPubs,fSize):
 	#for i in range(0,100,10):
 		t = strftime("%H:%M:%S", gmtime())
@@ -23,15 +24,76 @@ def get_xml():
 			print fName,'XML file already downloaded'
 		else:
 			w = gzip.open(dataDir+'/xml/'+fName,'w')
-			url = 'https://research-information.bris.ac.uk/ws/rest/publication?window.size='+str(fSize)+'&offset='+str(i)
-			#print url
+			#url = 'https://research-information.bris.ac.uk/ws/rest/publication?window.size='+str(fSize)+'&offset='+str(i)+'&rendering=xml_long'
+			url = 'https://research-information.bris.ac.uk/ws/rest/publication?window.size='+str(fSize)+'&window.offset='+str(i)+'&rendering=xml_short'
+			print url
 			res = requests.get(url)
 			w.write(res.text.encode('utf-8'))
 			w.close()
 
+def get_long():
+	print "Getting Pure long"
+	fSize=10000
+	for i in range(0,totalPubs,fSize):
+	#for i in range(0,100,10):
+		t = strftime("%H:%M:%S", gmtime())
+		print t,i
+		fName = 'pure_'+str(i)+'.long.gz'
+		if is_non_zero_file(dataDir+'/long/'+fName):
+			print fName,'long file already downloaded'
+		else:
+			w = gzip.open(dataDir+'/long/'+fName,'w')
+			url = 'https://research-information.bris.ac.uk/ws/rest/publication?window.size='+str(fSize)+'&window.offset='+str(i)+'&rendering=long'
+			print url
+			res = requests.get(url)
+			w.write(res.text.encode('utf-8'))
+			w.close()
+
+def parse_long():
+	fSize=10000
+	inAbs = False
+	absData = ''
+	uuid = ''
+	absDic = {}
+	o = gzip.open('data/absData.txt.gz','a')
+	for i in range(0,totalPubs,fSize):
+		fName = 'pure_'+str(i)+'.long.gz'
+		print "Reading "+fName
+		with gzip.open(dataDir+'/long/'+fName) as data_file:
+			for line in data_file:
+				lData = line.split('<')
+				for l in lData:
+					m = re.match(r'.*?renderedContentUUID="(.*?)".*',l)
+					if m:
+						#print m.group(1)
+						uuid = m.group(1)
+					aStart = re.match(r'div class="textblock">',l)
+					if aStart:
+						inAbs=True
+					aEnd = re.match(r'/div>',l)
+					if aEnd:
+						#clean up absData
+						absData = absData.replace('div class="textblock">','')
+						absData = re.sub(r'^p>','',absData)
+						absData = re.sub(r'/p>$','',absData)
+						absData = absData.replace('\n',' ')
+						#print absData
+						#check if already done
+						if uuid in absDic:
+							if len(absDic[uuid])<len(absData):
+								o.write(uuid+'\t'+absData+'\n')
+								absDic[uuid]=absData
+						else:
+							o.write(uuid+'\t'+absData+'\n')
+							absDic[uuid]=absData
+						absData = ''
+						inAbs=False
+					if inAbs == True:
+						absData+=l
+	o.close()
+
+
 def xml_to_json():
-	fSize=1000
-	totalPubs = 160000
 	for i in range(0,totalPubs,fSize):
 		fName_json = 'pure_'+str(i)+'.json.gz'
 		fName_xml = 'pure_'+str(i)+'.xml.gz'
@@ -46,9 +108,24 @@ def xml_to_json():
 					w.write(json.dumps(doc))
 				w.close()
 
+def parse_json():
+	fName = 'pure_0.json.gz'
+	with gzip.open(dataDir+'/json/'+fName) as data_file:
+		json_data = json.load(data_file)
+		for j in json_data['publication-template:GetPublicationResponse']['core:result']['core:content']:
+			uuid = title = abstract = ''
+			uuid = j['@uuid']
+			if 'publication-base_uk:title' in j:
+				title = j['publication-base_uk:title'].encode('utf-8')
+
+			print uuid,title,abstract
+
 def main():
-	get_xml()
+	#get_xml()
+	get_long()
+	#parse_long()
 	#xml_to_json()
+	#parse_json()
 
 if __name__ == '__main__':
 	main()
